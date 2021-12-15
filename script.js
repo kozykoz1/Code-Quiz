0bfb3687aa4c4e8ed203674f5f70501a315d6f59
kozykoz1 <93100015+kozykoz1@users.noreply.github.com>
2021-12-15 20:29:15 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,85 @@
+const quizData = [
+    {
+        question: "which language runs in a web browser",
+        a: "Java",
+        b: "css",
+        c: "Python",
+        d:"javascript",
+        correcct: "d"
+   
+    },
+    {
+        question: "what does css stand for",
+        a: "central...",
+        b: "cascading...",
+        c: "cascading simple..",
+        d: "cars..",
+        correct: "b",
+    }
+
+]
+
+const quiz = document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+
+
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
+
+function loadQuiz() {
+
+    deselectAnswers()
+
+    const currentQuizData = quizData[currentQuiz]
+
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+}
+
+function deselectAnswers() {
+    answerEls.forEach(answerEls => answerEls.checked = false)
+
+}
+
+function getSelected() {
+    let answerEls
+    answerEls.forEach(answerEls => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+    return answer
+}
+
+
+
+submitBtn.addEventListener('click',  () => {
+    const answer = getSelected()
+    if(answer) {
+        if(answer === quizData[currentQuiz].correct){
+
+        } 
+
+        currentQuiz++
+
+        if(currentQuiz < quizData.length) {
+            loadQuiz()
+        } else {
+            quiz.innerHTML = `
+            <h2>You answered ${score}/${quizData.length} questions correctly<h2>
+
+            <button onclick="location.reload()">Reload</button>`
+            
+        }
+    }
+})
